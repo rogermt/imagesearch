f30f9adcce62961c45dbbd8b2c783866a18996cd
rogermt <roger_taylor@ymail.com>
2016-09-03 17:29:07 +0000

    tidy up diff for deleted files

--- config/env/production.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    // Development configuration options
-    db: 'mongodb://rogermt:Tr4ff0rd@ds145315.mlab.com:45315/heroku_1dfrv356',
-    sessionSecret: 'productionSessionSecret'
-}
